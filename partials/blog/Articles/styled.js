d74facee6a2fa5adbe1f30bde865d6a5ee4dfd63
Helena Martins <lenatinsjs@gmail.com>
2022-12-27 21:06:11 -0300

    fix: grid on mobile - blog

--- partials/blog/Articles/styled.js
+++ partials/blog/Articles/styled.js
@@ -2,11 +2,18 @@ import styled from "styled-components";
 import { device } from "../../../styles/breakpoints";
 
 export const Articles = styled.section`
-  padding: 100px 0;
+  padding-bottom: 50px;
+
+  @media ${device.tabletL} {
+    padding: 100px 0;
+  }
 `;
 
 export const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
   grid-gap: 20px;
+
+  @media ${device.tabletL} {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
